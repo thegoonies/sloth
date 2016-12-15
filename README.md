8d967a6d8908df53ec9d6b261289e82065bbea56
hugsy <hugsy@blah.cat>
2016-12-15 10:24:02 +0900

    :beer:

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-# pwnbox
+## Sloth - TheGoonies CTF PwnBox
+
+### Ready
 
 Build a CTF-ready VM in 5 min using [`Vagrant`](https://www.vagrantup.com/)
 
+### Set
 Simply clone this repo and run:
 ```bash
 $ vagrant up --provision && vagrant ssh
 ```
 
+### Pwn 
+![](https://i.imgur.com/BXaKhKO.jpg)
--- trinity_install.sh
+++ trinity_install.sh
@@ -3,10 +3,13 @@
 ## capstone-engine
 cd /tmp
 git clone https://github.com/aquynh/capstone.git
-cd capstone && mkdir build && cd !$
+cd capstone && mkdir build && cd build
 cmake .. && make install && cd ..
 cd bindings/python
 make install install3
+# quick n dirty fix but please feel free to rtfm/improve
+ln -s /usr/local/lib/libcapstone.so /usr/local/lib/python2.7/dist-packages/capstone-3.0.4-py2.7.egg/capstone/libcapstone.so
+ln -s /usr/local/lib/libcapstone.so /usr/local/lib/python3.4/dist-packages/capstone-3.0.4-py3.4.egg/capstone/libcapstone.so
 
 ## keystone-engine
 cd /tmp
@@ -17,6 +20,11 @@ cd bindings/python
 make install install3
 
 ## unicorn-engine
-# todo
+sudo apt-get install pkg-config libglib2.0-dev -y
+cd /tmp
+git clone https://github.com/unicorn-engine/unicorn.git
+cd unicorn && ./make.sh && ./make.sh install
+cd bindings/python
+make install install3
 
-rm -fr /tmp/capstone /tmp/keystone
+rm -fr /tmp/capstone /tmp/keystone /tmp/unicorn
